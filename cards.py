daf01d76b008a93d04545f2b6c74b19880e77afe
Maks Biski <maksde25@gmail.com>
2023-01-14 19:40:37 +0100

    Implemented card class

--- cards.py
+++ cards.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-STARTING_FACTOR_FRACTION = 2.5
+STARTING_EASE = 2.5
 
 
 @dataclass(order=True)
@@ -9,5 +9,18 @@ class Card:
     back: str
     due: int = 0
     interval: int = 1
-    ease_factor: float = STARTING_FACTOR_FRACTION
+    ease_factor: float = STARTING_EASE
     reps: int = 0
+
+    @classmethod
+    def from_json(cls, json_data):
+        front = json_data['front']
+        back = json_data['back']
+        due = json_data['due']
+        interval = json_data['interval']
+        ease_factor = json_data['ease_factor']
+        reps = json_data['reps']
+        return cls(front, back, due, interval, ease_factor, reps)
+
+    def __str__(self):
+        return f'Card Front: {self.front}\nCard back: {self.back}\nCard is due: {self.due}\nCurrent interval: {self.interval}\nCurrent ease_factor: {self.ease_factor}\nTimes this card has been answered correctly in a row: {self.reps}'
